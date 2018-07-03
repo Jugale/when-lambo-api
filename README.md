8e482a411b75ae24cc8587662b4907c56b7ba9d8
Max Chuquimia <max@chuquimianproductions.com>
2018-07-03 20:05:42 +1000

    Working

---
+++ README.md
@@ -0,0 +1,64 @@
+# “When Lambo?” (as a service)
+Given a cryptocurrency code and the amount of it you are holding, returns whether you can buy a Lamborghini or not.
+
+[Try a live demo](when-lambo.herokuapp.com/api)
+
+## Usage
+The API has a single endpoint requiring two query parameters:
+```
+/api/?coin={COIN}&amount={AMOUNT}
+```
+
+Response model:
+```
+{
+    "when_lambo": String // "not yet" or "now"
+}
+```
+
+### Example
+
+```
+$ curl -i "when-lambo.herokuapp.com/api?coin=BTC&amount=0.5"
+HTTP/1.1 200 OK
+Etag: 8d988e0ffcfd1c5639d80e5f0b7b8dfb
+Content-Type: application/json; charset=utf-8
+Content-Length: 24
+...
+
+{"when_lambo":"not yet"}
+```
+
+### Entity Tagging
+
+To reduce load on your client, you can use the `ETag` header from the response above  in an `If-None-Match` header in your request to get a `304 Not Modified` when the response entity hasn’t changed for the given request.
+
+```
+$ curl -i -H "If-None-Match: 4caf48a872d2ab065eff31edbf74c5fe" "when-lambo.herokuapp.com/api?coin=DENT&amount=40000"
+HTTP/1.1 304 Not Modified
+Content-Length: 0
+...
+```
+
+### Errors
+
+Invalid requests will return an `‘application/problem+json` entity:
+```
+$ curl "when-lambo.herokuapp.com/api?coin=nonexistantCoin&amount=1" -i
+HTTP/1.1 400 Bad Request
+Content-Type: application/problem+json; charset=utf-8
+Content-Length: 55
+...
+
+{"error":"unable to retrieve price of NONEXISTANTCOIN"}
+```
+
+## How much is a Lamborghini anyway?
+I wasn’t sure so I asked [Wolfram|Alpha](http://www.wolframalpha.com/input/?i=lamborghini+cost+2018&assumption=%7B%22C%22,+%22lamborghini%22%7D+-%3E+%7B%22AutomobileManufacturer%22%7D&assumption=%7B%22DPClash%22,+%22AutomobileManufacturerP%22,+%22cost%22%7D+-%3E+%7B%22MSRPPrice%22%7D). The API uses the constant $ for calculations.
+
+## Motivation 
+I accidentally came across [this challenge](https://hackclub.com/workshops/challenge_ridiculous_api/) - and who can say no to a good challenge? Also I have very little NodeJS experience so it was a good learning opportunity. 
+
+## Licence
+MIT
+
--- index.js
+++ index.js
@@ -3,7 +3,106 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const request = require('request');
+const md5 = require('md5');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+function handleAPI(req, res) {
+
+    var coinCode = req.query.coin;
+    var amount = req.query.amount;
+    var ETag = req.header("If-None-Match");
+
+    if (!coinCode) {
+        res.status(400);
+        respondWithError("missing parameter 'coin'", res);
+        return;
+    }
+
+    if (!amount) {
+        res.status(400);
+        respondWithError("missing parameter 'amount'", res);
+        return;
+    } else {
+        amount = parseFloat(amount);
+    }
+
+    var COINCODE = coinCode.toUpperCase();
+    var crunchFunction = crunch;
+    var errorFunction = respondWithError;
+
+    console.log(`Data: coin=${COINCODE}, amount=${amount}`);
+
+    request({
+        url: `https://min-api.cryptocompare.com/data/price?fsym=${COINCODE}&tsyms=USD`,
+        json: true
+      }, function (error, response, body) {
+
+        if (body.Response == "Error") {
+            res.status(400);
+            console.log(`Coin Error: ${body}`);
+            errorFunction(`unable to retrieve price of ${COINCODE}`, res);
+            return;
+        }
+
+        var USD_price = body.USD;
+        console.log(`Coin price: ${USD_price}`);
+
+        crunchFunction(USD_price, amount, ETag, COINCODE, res);
+    });
+}
+
+function crunch(USD, amount, ETag, coin, res) {
+
+    var savings = USD * amount;
+    console.log(`Savings: ${savings}`);
+
+    // From http://www.wolframalpha.com/input/?i=lamborghini+cost+2018&assumption=%7B%22C%22,+%22lamborghini%22%7D+-%3E+%7B%22AutomobileManufacturer%22%7D&assumption=%7B%22DPClash%22,+%22AutomobileManufacturerP%22,+%22cost%22%7D+-%3E+%7B%22MSRPPrice%22%7D on 2018-07-03
+    var lamboPrice = 296100.0;
+
+    var responseString = "unknown";
+
+    // Business Logic
+    if (savings < lamboPrice) {
+        responseString = "not yet";
+    } else {
+        responseString = "now";
+    }
+
+    // Entity tagging, just for the lols
+    var calculatedTag = md5(`${coin}-${amount}-${responseString}`);
+
+    if (ETag) {
+        if (calculatedTag == ETag) {
+            // The response hasn't changed
+            // 304 Not Modified
+            res.status(304);
+            res.set('ETag', '');
+            res.send('');
+            return;
+        }
+    }
+
+    res.status(200);
+    res.set('ETag', calculatedTag);
+    respondWith(responseString, res);
+}
+
+function respondWithError(message, res) {
+    var obj = { "error": message };
+    var jsonString = JSON.stringify(obj);
+
+    res.set('Content-Type', 'application/problem+json');
+    res.send(jsonString);
+}
+
+function respondWith(when_lambo, res) {
+    var obj = { "when_lambo": when_lambo };
+    var jsonString = JSON.stringify(obj);
+
+    res.set('Content-Type', 'application/json');
+    res.send(jsonString);
+}
+
+app.get('/api/', handleAPI);
 
 app.listen(PORT, () => console.log('Example app listening on port: ' + PORT));
